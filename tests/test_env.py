5e465df9c8c7d7078500c97baa7de7fb9bd39bf2
Saatwik <satwiktiwari59@gmail.com>
2025-04-24 10:03:58 +0530

    new xml model

--- tests/test_env.py
+++ tests/test_env.py
@@ -2,6 +2,8 @@ import pytest
 from gym_env.custom_env import CustomMujocoEnv
 
 def test_env_init():
-    env = CustomMujocoEnv('mujoco/mybot.xml', render_mode='rgb_array')
+    env = CustomMujocoEnv('mujoco/quadruped_go2_like_generated.xml', render_mode='rgb_array')
     obs, _ = env.reset()
     assert env.observation_space.contains(obs)
+    assert env.action_space.contains(env.action_space.sample())
+    env.close()
