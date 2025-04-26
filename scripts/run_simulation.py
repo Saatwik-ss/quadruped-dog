ec6f826802c855e7dbeebcebf3cf6ccfd06536d5
Saatwik <satwiktiwari59@gmail.com>
2025-04-26 20:13:27 +0530

    new model

--- scripts/run_simulation.py
+++ scripts/run_simulation.py
@@ -2,14 +2,17 @@
 import mujoco
 import mujoco.viewer as viewer
 from pathlib import Path
+from gym_env.custom_env import calc_done
 
 def main():
-    xml_path = Path(__file__).parent.parent / "mujoco" / "go2w.xml"
-    # load the model + data
+    xml_path = Path(__file__).parent.parent / "mujoco" / "car.xml"
     model = mujoco.MjModel.from_xml_path(str(xml_path))
     data  = mujoco.MjData(model)
-    # launch the full GUI
     viewer.launch(model, data)
 
 if __name__ == "__main__":
     main()
+    
+"""
+python -m scripts.run_simulation
+"""
