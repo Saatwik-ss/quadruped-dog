618572f39442fc81550c1d1f00e971d6685a71dc
Saatwik <satwiktiwari59@gmail.com>
2025-04-26 21:51:25 +0530

    Update go2_bridge.py

--- deployment/go2_bridge.py
+++ deployment/go2_bridge.py
@@ -5,7 +5,7 @@ import mujoco
 import mujoco.viewer as viewer
 from pathlib import Path
 
-xml = Path("scripts\mujoco\mybot.xml") 
+xml = Path("scripts\mujoco\car.xml") 
 model = mujoco.MjModel.from_xml_path(str(xml))
 data  = mujoco.MjData(model)
 viewer.launch(model, data)
